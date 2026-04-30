3748a58ee3016085a9142808778b185f34eac4cb
Ismael <ismael@example.com>
2026-04-30 12:18:45 +0200

    estructuracion y organizacion

--- src/app.js
+++ src/app.js
@@ -2,13 +2,18 @@
 
 const express = require("express")
 const routes = require("./routes")
+const loggerMiddleware = require("./middlewares/logger")
+const errorMiddleware = require("./middlewares/errorHandler")
 const app = express()
 
 app.use(express.json())
+app.use(loggerMiddleware)
 app.use("/api", routes) // las rutas serian /api/...
 
 app.get("/", (req, res) => {
     res.send("Hello world")
 })
+
+app.use(errorMiddleware)
 
 module.exports = app
