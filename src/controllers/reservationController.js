8086ff873b442adbdeb7c77d1cc2f6cc53c8df9e
Ismael <ismael@example.com>
2026-05-05 12:11:43 +0200

    controladores para reservas

---
+++ src/controllers/reservationController.js
@@ -0,0 +1,44 @@
+exports.createReservation = async (req, res) => {
+    try{
+        const reservations = await reservationServices.createReservation(req.body)
+        return res.status(201).json(reservations)
+    } catch (error) {
+        return res.status(400).json({error: error.message})
+    }
+}
+
+exports.getReservation = async (req, res) => {
+    try{
+        const reservation = await reservationServices.getReservation(req.params.id)
+        if(!reservation) {
+            return res.status(404).json({error: "Reservation not found"})
+        }
+        return res.status(201).json(reservation)
+    } catch (error) {
+        return res.status(400).json({error: error.message})
+    }
+}
+
+exports.updateReservation = async (req, res) => {
+    try{
+        const updateReservation = await reservationServices.updateReservation(req.body, req.params.id)
+        if(!updateReservation) {
+            return res.status(404).json( updateReservation )
+        }
+        return res.status(201).json({message: "User update succesfully"})
+    } catch (error) {
+        return res.status(400).json({error: error.message})
+    }
+}
+
+exports.deleteReservation = async (req, res) => {
+    try{
+        const deleteReservation = await reservationServices.deleteReservation(req.params.id)
+        if(!deleteReservation) {
+            return res.status(404).json({error: "Reservation not found"})
+        }
+        return res.status(204).send()
+    } catch (error) {
+        return res.status(400).json({error: error.message})
+    }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,12 @@
 const { Router } = require("express")
 const authRouter = require("./auth")
 const adminRouter = require("./admin")
+const reservationRouter = require("./reservation")
 
 const router = Router();
 
 router.use("/auth", authRouter) // dentro de /auth tenemos las rutas que pertenecen a ese grupo
 router.use("/admin", adminRouter)
+router.use("/reservations", reservationRouter)
 
 module.exports = router
