34afa44f5ec46c444452a6c116a6146328d7a6b3
Ismael <ismael@example.com>
2026-05-13 11:54:15 +0200

    configuracion de cors 2

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,26 +1,21 @@
-// app.js es el archivo donde se configura la aplicación Express: middlewares, rutas y endpoints.
-
-const express = require("express")
+const express = require("express");
 const cors = require("cors");
-const routes = require("./routes")
-const loggerMiddleware = require("./middlewares/logger")
-const errorMiddleware = require("./middlewares/errorHandler")
-const app = express()
+const routes = require("./routes");
+const loggerMiddleware = require("./middlewares/logger");
+const errorMiddleware = require("./middlewares/errorHandler");
+const app = express();
 
-app.use(cors({
-  origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true
-}));
+app.use(cors());
+app.options("*", cors());
 
-app.use(express.json())
-app.use(loggerMiddleware)
-app.use("/api", routes) // las rutas serian /api/...
+app.use(express.json());
+app.use(loggerMiddleware);
+app.use("/api", routes);
 
 app.get("/", (req, res) => {
-    res.send("Hello world")
-})
+  res.send("Hello world");
+});
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
-module.exports = app
+module.exports = app;
