cde66dfa5f91c7c6971c7c91f912e72ebe7f7ce2
Ismael <ismael@example.com>
2026-06-03 11:30:15 +0200

    controlador y servicio de editar usuario

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,6 +1,6 @@
 //La carpeta controller se encarga de manejar la petición del cliente, llamar a las funciones de la capa service (donde está la lógica de negocio) y devolver la respuesta al cliente.
 
-const { registerUser, loginUser} = require("../services/authServices")
+const { registerUser, loginUser, editUser} = require("../services/authServices")
 
 
 const register = async (req, res) => {
@@ -23,4 +23,19 @@ const login = async (req, res) => {
     }
 }
 
-module.exports = {register, login}
+const edit = async (req, res) => {
+
+    try{
+
+        const { id } = req.params;
+        const { name, email, password } = req.body
+        const updated = await editUser(id, {name, email, password})
+        return res.json(updated)
+
+    }catch (error){
+        return res.status(400).json({ error: error.message });
+    }
+
+}
+
+module.exports = {register, login, edit}
--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,13 +1,14 @@
 // auth.js define las rutas relacionadas con la autenticación (login, register, rutas protegidas) y conecta esas rutas con los controllers y middlewares correspondientes.
 
 const { Router } = require("express")
-const { register, login} = require("../controllers/authController")
+const { register, login, edit} = require("../controllers/authController")
 const authenticateToken = require("../middlewares/auth")
 
 const router = Router()
 
 router.post("/register", register);
 router.post("/login", login)
+router.put("/edit-user", edit)
 
 router.get("/protected-route", authenticateToken, (req, res) => {
     res.send("Esta es una ruta protegida")
